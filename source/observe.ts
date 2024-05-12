f6240c7e529423f11e0a6ec358fc6d5a760c0e48
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-05-12 17:22:20 +0200

    fix: Hardcoded path prefix

--- source/observe.ts
+++ source/observe.ts
@@ -6,11 +6,11 @@ import { argv } from "node:process";
 
 const DEBIAN_COMPONENT = process.env.DEBIAN_COMPONENT ?? "main";
 const DEBIAN_OBSERVABLES = process.env.DEBIAN_OBSERVABLES?.split("\n").filter(Boolean) ?? [];
-const OUTPUT_DIRECTORY = process.env.OUTPUT_DIRECTORY ?? argv[2] ?? process.cwd();
+const OUTPUT_DIRECTORY = `${process.env.OUTPUT_DIRECTORY ?? argv[2] ?? process.cwd()}/apt`;
 
 const main = async () => {
   console.log(
-    `Merging '${DEBIAN_OBSERVABLES.length.toString()}' observables into '${OUTPUT_DIRECTORY}/apt'...`,
+    `Merging '${DEBIAN_OBSERVABLES.length.toString()}' observables into '${OUTPUT_DIRECTORY}'...`,
   );
 
   await mkdir(OUTPUT_DIRECTORY, { recursive: true });
