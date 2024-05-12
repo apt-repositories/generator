1a8b6a9eb39147ce81ed6036df260dd4962eea48
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-05-12 15:48:31 +0200

    feat: Improved logging

--- source/validate.ts
+++ source/validate.ts
@@ -6,29 +6,39 @@ import { argv } from "node:process";
 
 const OUTPUT_DIRECTORY = process.env.OUTPUT_DIRECTORY ?? argv[2] ?? process.cwd();
 
-const validateJsonRecursive = async (root: string) => {
+const validateJsonRecursive = async (root: string): Promise<[boolean, number]> => {
   const files = await readdir(root);
   let hasFailed = false;
+  let checkedCount = 0;
   for (const file of files) {
     const path = join(root, file);
     const pathStat = await stat(path);
     if (pathStat.isDirectory()) {
-      await validateJsonRecursive(path);
-    } else {
-      const content = await readFile(path);
-      try {
-        JSON.parse(content.toString());
-      } catch (error) {
-        console.error(`Failed to parse JSON file '${path}'`);
+      const [subFailed, subCount] = await validateJsonRecursive(path);
+      if (!hasFailed && subFailed) {
         hasFailed = true;
       }
+      checkedCount += subCount;
+      continue;
     }
+
+    const content = await readFile(path);
+    try {
+      JSON.parse(content.toString());
+    } catch (error) {
+      console.error(`Failed to parse JSON file '${path}'`);
+      hasFailed = true;
+    }
+    ++checkedCount;
   }
-  return hasFailed;
+
+  return [hasFailed, checkedCount];
 };
 
 const main = async () => {
-  const hasFailed = await validateJsonRecursive(OUTPUT_DIRECTORY);
+  const [hasFailed, checkedCount] = await validateJsonRecursive(OUTPUT_DIRECTORY);
+
+  console.log(`Checked '${checkedCount.toString()}' files in '${OUTPUT_DIRECTORY}'.`);
 
   if (hasFailed) {
     throw new Error("Validation failed.");
