8c2d29c2802a9529f7744fc61ea37cbcbd5647b3
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-05-14 12:45:41 +0200

    chore: Clean up obsolete configs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 /** @type {import("lint-staged").Config} */
 export default {
   "package.json": "yarn prettier-package-json --write",
-  "!(apt/**/*)*.{js,json,md,sh,ts,yml}": "prettier --write",
+  "*.{js,json,md,sh,ts,yml}": "prettier --write",
 };
