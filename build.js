f7303aacc7cd5a07f860230bcbe39b98f88a7501
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-07-03 18:45:36 +0200

    fix: Redundant external

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ esbuild
   .build({
     bundle: true,
     entryPoints: ["./source/generate.ts", "./source/observe.ts", "./source/validate.ts"],
-    external: ["node:assert", "xz-decompress"],
+    external: ["xz-decompress"],
     format: "esm",
     inject: ["source/cjs-shim.ts"],
     outdir: "./output",
