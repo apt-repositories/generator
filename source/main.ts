98a6414c8923a1938b528e37ad62bf2218a23e29
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-02-25 10:57:32 +0100

    fix: Warning message

--- source/main.ts
+++ source/main.ts
@@ -132,15 +132,13 @@ const main = async () => {
 
       if (0 === packageCountComponent) {
         process.stderr.write(
-          outdent`
+          outdent`  !
+          
+            If it is reasonable to expect that this component will never have any entries, mark the component as empty in your configuration:
 
-          If it is reasonable to expect that this component will never have any entries, mark the component as empty in your configuration:
+            emptyComponents: { "${task.release}": ["${task.component}"] }
 
-          emptyComponents: {
-            "${task.release}": ["${task.component}"],
-          },
-
-          `,
+            !`,
         );
       }
     }
